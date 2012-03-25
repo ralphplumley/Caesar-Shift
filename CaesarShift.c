34c928023b95ea7c1b45ea37b52f5c2b2d2dfed2
Ralph Tomio <ralph.tomio@gmail.com>
2012-03-25 12:38:58 -0400

    Initial Commit

---
+++ CaesarShift.c
@@ -0,0 +1,19 @@
+// Caesar Shift, in C.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, const char* argv[]) {
+
+	int shift = atoi(argv[1]);
+	char message[256];
+	strcpy(message, argv[2]);
+	
+	int i;
+	for(i = 0; i < strlen(message); i++) {
+		printf("%c", message[i] + shift);
+	}
+	putchar('\n');
+	return 0;
+}
